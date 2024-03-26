1fb486037e8a48bbc298785f746ba42d44de57ad
WSR-1 <iwsr1@mail.ru>
2024-03-26 17:28:34 +0800

    sdfasf

--- app/src/main/java/com/example/wsr/MainActivity.java
+++ app/src/main/java/com/example/wsr/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.wsr;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,27 +11,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.wsr_3.R;
 
+public class MainActivity extends AppCompatActivity {
     /*
-    spashscreen
-    2.03.2024
+    окно регистрации или фхода
+    26.03.2024
     Суриков Владислав
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        final Button singupp = findViewById(R.id.button), signin = findViewById(R.id.button2);
+        singupp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, signup.class);
+                startActivity(intent); // переход на регестрацию
+            }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, signin.class);
+                startActivity(intent); // переход на вход
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void onboard1(View v){
-        Intent intent = new Intent(MainActivity.this, onboarding.class);
-        startActivity(intent);
-    }
-
 }
