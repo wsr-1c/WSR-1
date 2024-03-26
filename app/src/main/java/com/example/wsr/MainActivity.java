412ca20714b8885b7e569783e6fb6fac6d620efc
WSR-1 <iwsr1@mail.ru>
2024-03-26 12:02:48 +0800

    da

---
+++ app/src/main/java/com/example/wsr/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.wsr;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    /*spashscreen
+    26.03.2024
+    Суриков Владислав
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void onboard(){
+        Intent intent = new Intent(MainActivity.this, onboarding.class);
+        startActivity(intent);
+        // переключение на онбоард
+    }
+}
